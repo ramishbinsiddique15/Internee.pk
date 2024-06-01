44cbd2809fdb535083cb032ebec6e142827c0f7f
Ramish Bin Siddique <ramishbinsiddique24@gmail.com>
2024-06-01 12:25:45 +0500

    Refaxtoring

--- src/components/LMS.jsx
+++ src/components/LMS.jsx
@@ -51,6 +51,28 @@ const LMS = () => {
     }
   };
 
+  const options = [
+    {
+      iconPath: "M19 18H6C3.79 18 2 16.21 2 14C2 11.79 3.79 10 6 10H6.71C7.37 7.69 9.5 6 12 6C15.04 6 17.5 8.46 17.5 11.5V12H19C20.66 12 22 13.34 22 15C22 16.66 20.66 18 19 18ZM19.35 10.03C18.67 6.59 15.64 4 12 4C9.11 4 6.6 5.64 5.35 8.03C2.34 8.36 0 10.9 0 14C0 17.31 2.69 20 6 20H19C21.76 20 24 17.76 24 15C24 12.36 21.95 10.22 19.35 10.03ZM10 17L6.5 13.5L7.91 12.08L10 14.17L14.59 9.59L16 11",
+      title: "Sell Courses and Earn",
+      description: "Are you an expert in your field? Share your knowledge on our LMS. Create and sell courses, or contribute as an instructor. Empower others on their learning journey while earning rewards for your expertise."
+    },
+    {
+      iconPath: "M15.5 14L20.5 19L19 20.5L14 15.5V14.71L13.73 14.43C12.59 15.41 11.11 16 9.5 16C7.77609 16 6.12279 15.3152 4.90381 14.0962C3.68482 12.8772 3 11.2239 3 9.5C3 7.77609 3.68482 6.12279 4.90381 4.90381C6.12279 3.68482 7.77609 3 9.5 3C11.2239 3 12.8772 3.68482 14.0962 4.90381C15.3152 6.12279 16 7.77609 16 9.5C16 11.11 15.41 12.59 14.43 13.73L14.71 14H15.5ZM9.5 14C12 14 14 12 14 9.5C14 7 12 5 9.5 5C7 5 5 7 5 9.5C5 12 7 14 9.5 14ZM12 10H10V12H9V10H7V9H9V7H10V9H12V10Z",
+      title: "Certification",
+      description: "Complete courses on our LMS and earn certifications that validate your expertise. Showcase your accomplishments to potential employers and stand out in a competitive landscape."
+    },
+    {
+      iconPath: "M11 9H13V7H11V9ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM12 2C10.6868 2 9.38642 2.25866 8.17317 2.7612C6.95991 3.26375 5.85752 4.00035 4.92893 4.92893C3.05357 6.8043 2 9.34784 2 12C2 14.6522 3.05357 17.1957 4.92893 19.0711C5.85752 19.9997 6.95991 20.7362 8.17317 21.2388C9.38642 21.7413 10.6868 22 12 22C14.6522 22 17.1957 20.9464 19.0711 19.0711C20.9464 17.1957 22 14.6522 22 12C22 10.6868 21.7413 9.38642 21.2388 8.17317C20.7362 6.95991 19.9997 5.85752 19.0711 4.92893C18.1425 4.00035 17.0401 3.26375 15.8268 2.7612C14.6136 2.25866 13.3132 2 12 2V2ZM11 17H13V11H11V17Z",
+      title: "Courses in Urdu",
+      description: "Dive into the world of knowledge with our courses in Urdu. Breaking language barriers, Our LMS ensures that education is accessible and relatable for everyone. Learn, grow, and excel in a language that feels like home."
+    },
+    {
+      iconPath: "M22.5625 4.01453C23.0929 4.01453 23.6016 4.22524 23.9767 4.60031C24.3518 4.97539 24.5625 5.48409 24.5625 6.01453V16.0145C24.5625 16.545 24.3518 17.0537 23.9767 17.4287C23.6016 17.8038 23.0929 18.0145 22.5625 18.0145H6.5625C6.03207 18.0145 5.52336 17.8038 5.14829 17.4287C4.77321 17.0537 4.5625 16.545 4.5625 16.0145V4.01453C4.5625 3.48409 4.77321 2.97539 5.14829 2.60031C5.52336 2.22524 6.03207 2.01453 6.5625 2.01453H12.5625L14.5625 4.01453H22.5625ZM2.5625 6.01453V20.0145H20.5625V22.0145H2.5625C2.03207 22.0145 1.52336 21.8038 1.14829 21.4287C0.773214 21.0537 0.5625 20.545 0.5625 20.0145V11.0145V6.01453H2.5625ZM6.5625 6.01453V16.0145H22.5625V6.01453H6.5625Z",
+      title: "Practice Exercises",
+      description: "Theory is just the beginning. Our LMS goes beyond by offering practical exercises that challenge and refine your skills. Apply your knowledge in real-world scenarios, solidifying your understanding and boosting your confidence"
+    }
+  ];
   return (
     <div className="Portal">
       
@@ -60,67 +82,20 @@ const LMS = () => {
         <p>
         Want to learn something but don't know what's the roadmap or your english is not too good? That's why we launch LMS for you.   </p>
         <div className="options">
-          <div className="option">
-            <div className="icon" ref={addIconRef}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M19 18H6C3.79 18 2 16.21 2 14C2 11.79 3.79 10 6 10H6.71C7.37 7.69 9.5 6 12 6C15.04 6 17.5 8.46 17.5 11.5V12H19C20.66 12 22 13.34 22 15C22 16.66 20.66 18 19 18ZM19.35 10.03C18.67 6.59 15.64 4 12 4C9.11 4 6.6 5.64 5.35 8.03C2.34 8.36 0 10.9 0 14C0 17.31 2.69 20 6 20H19C21.76 20 24 17.76 24 15C24 12.36 21.95 10.22 19.35 10.03ZM10 17L6.5 13.5L7.91 12.08L10 14.17L14.59 9.59L16 11" fill="#43a724"/>
-</svg>
-
+          {options.map((option, index) => (
+            <div className="option" key={index}>
+              <div className="icon" ref={addIconRef}>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d={option.iconPath} fill="#43a724" />
+                </svg>
+              </div>
+              <div className="option-txt">
+                <p>
+                  <b>{option.title}</b> {option.description}
+                </p>
+              </div>
             </div>
-            <div className="option-txt">
-              <p>
-                <b>Sell Courses and Earn</b> Are you an expert in your field? Share your knowledge on our LMS. Create and sell courses, or contribute as an instructor. Empower others on their learning journey while earning rewards for your expertise.
-              </p>
-            </div>
-          </div>
-          <div className="option">
-            <div className="icon" ref={addIconRef}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M15.5 14L20.5 19L19 20.5L14 15.5V14.71L13.73 14.43C12.59 15.41 11.11 16 9.5 16C7.77609 16 6.12279 15.3152 4.90381 14.0962C3.68482 12.8772 3 11.2239 3 9.5C3 7.77609 3.68482 6.12279 4.90381 4.90381C6.12279 3.68482 7.77609 3 9.5 3C11.2239 3 12.8772 3.68482 14.0962 4.90381C15.3152 6.12279 16 7.77609 16 9.5C16 11.11 15.41 12.59 14.43 13.73L14.71 14H15.5ZM9.5 14C12 14 14 12 14 9.5C14 7 12 5 9.5 5C7 5 5 7 5 9.5C5 12 7 14 9.5 14ZM12 10H10V12H9V10H7V9H9V7H10V9H12V10Z" fill="#43a724"/>
-</svg>
-
-            </div>
-            <div className="option-txt">
-              <p>
-                <b>Certification</b> Complete courses on our LMS and earn certifications that validate your expertise. Showcase your accomplishments to potential employers and stand out in a competitive landscape.
-              </p>
-            </div>
-          </div>
-          <div className="option">
-            <div className="icon" ref={addIconRef}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M11 9H13V7H11V9ZM12 20C7.59 20 4 16.41 4 12C4 7.59 7.59 4 12 4C16.41 4 20 7.59 20 12C20 16.41 16.41 20 12 20ZM12 2C10.6868 2 9.38642 2.25866 8.17317 2.7612C6.95991 3.26375 5.85752 4.00035 4.92893 4.92893C3.05357 6.8043 2 9.34784 2 12C2 14.6522 3.05357 17.1957 4.92893 19.0711C5.85752 19.9997 6.95991 20.7362 8.17317 21.2388C9.38642 21.7413 10.6868 22 12 22C14.6522 22 17.1957 20.9464 19.0711 19.0711C20.9464 17.1957 22 14.6522 22 12C22 10.6868 21.7413 9.38642 21.2388 8.17317C20.7362 6.95991 19.9997 5.85752 19.0711 4.92893C18.1425 4.00035 17.0401 3.26375 15.8268 2.7612C14.6136 2.25866 13.3132 2 12 2V2ZM11 17H13V11H11V17Z" fill="#43a724"/>
-</svg>
-
-
-            </div>
-            <div className="option-txt">
-              <p>
-                <b>Courses in Urdu</b> Understanding the Software Development Life Cycle (SDLC) is pivotal in the tech world. Acquire skills that align with industry standards and boost your project management proficiency.
-              </p>
-            </div>
-          </div>
-          <div className="option">
-            <div className="icon" ref={addIconRef}>
-            <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0)">
-<path d="M22.5625 4.01453C23.0929 4.01453 23.6016 4.22524 23.9767 4.60031C24.3518 4.97539 24.5625 5.48409 24.5625 6.01453V16.0145C24.5625 16.545 24.3518 17.0537 23.9767 17.4287C23.6016 17.8038 23.0929 18.0145 22.5625 18.0145H6.5625C6.03207 18.0145 5.52336 17.8038 5.14829 17.4287C4.77321 17.0537 4.5625 16.545 4.5625 16.0145V4.01453C4.5625 3.48409 4.77321 2.97539 5.14829 2.60031C5.52336 2.22524 6.03207 2.01453 6.5625 2.01453H12.5625L14.5625 4.01453H22.5625ZM2.5625 6.01453V20.0145H20.5625V22.0145H2.5625C2.03207 22.0145 1.52336 21.8038 1.14829 21.4287C0.773214 21.0537 0.5625 20.545 0.5625 20.0145V11.0145V6.01453H2.5625ZM6.5625 6.01453V16.0145H22.5625V6.01453H6.5625Z" fill="#43a724"/>
-</g>
-<defs>
-<clipPath id="clip0">
-<rect width="24" height="24" fill="white" transform="translate(0.5625 0.0145264)"/>
-</clipPath>
-</defs>
-</svg>
-
-
-            </div>
-            <div className="option-txt">
-              <p>
-                <b>Practice Exercises</b> Theory is just the beginning. Our LMS goes beyond by offering practical exercises that challenge and refine your skills. Apply your knowledge in real-world scenarios, solidifying your understanding and boosting your confidence
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <div className="lms-img ">
--- src/components/Portal.jsx
+++ src/components/Portal.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import "./Portal.css";
-import task from ".././assets/img/task.png";
+import task from "../assets/img/task.png";
 
 const Portal = () => {
   const imgRef = useRef();
@@ -10,11 +10,7 @@ const Portal = () => {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("animate");
-          } else {
-            entry.target.classList.remove("animate");
-          }
+          entry.target.classList.toggle("animate", entry.isIntersecting);
         });
       },
       {
@@ -24,24 +20,12 @@ const Portal = () => {
       }
     );
 
-    if (imgRef.current) {
-      observer.observe(imgRef.current);
-    }
-    iconRefs.current.forEach((ref) => {
-      if (ref) {
-        observer.observe(ref);
-      }
-    });
+    if (imgRef.current) observer.observe(imgRef.current);
+    iconRefs.current.forEach((ref) => ref && observer.observe(ref));
 
     return () => {
-      if (imgRef.current) {
-        observer.unobserve(imgRef.current);
-      }
-      iconRefs.current.forEach((ref) => {
-        if (ref) {
-          observer.unobserve(ref);
-        }
-      });
+      if (imgRef.current) observer.unobserve(imgRef.current);
+      iconRefs.current.forEach((ref) => ref && observer.unobserve(ref));
     };
   }, []);
 
@@ -51,94 +35,53 @@ const Portal = () => {
     }
   };
 
+  const options = [
+    {
+      iconPath: "M2 3H10C10 2.46957 10.2107 1.96086 10.5858 1.58579C10.9609 1.21071 11.4696 1 12 1C12.5304 1 13.0391 1.21071 13.4142 1.58579C13.7893 1.96086 14 2.46957 14 3H22V5H21V16H15.25L17 22H15L13.25 16H10.75L9 22H7L8.75 16H3V5H2V3ZM5 5V14H19V5H5ZM11.85 11.85C11.76 11.94 11.64 12 11.5 12C11.3674 12 11.2402 11.9473 11.1464 11.8536C11.0527 11.7598 11 11.6326 11 11.5V7.5C11 7.36739 11.0527 7.24021 11.1464 7.14645C11.2402 7.05268 11.3674 7 11.5 7C11.64 7 11.76 7.06 11.85 7.15L13.25 8.54C13.57 8.86 13.89 9.18 13.89 9.5C13.89 9.82 13.57 10.14 13.25 10.46L11.85 11.85Z",
+      title: "Hands on Projects",
+      description: "We believe in learning by doing. Dive into hands-on projects that simulate real-world scenarios. From coding challenges to creative projects, every task is crafted to impart practical skills that resonate in professional environments."
+    },
+    {
+      iconPath: "M13 11C13.5933 11 14.1734 10.8241 14.6667 10.4944C15.1601 10.1648 15.5446 9.69623 15.7716 9.14805C15.9987 8.59987 16.0581 7.99667 15.9424 7.41473C15.8266 6.83279 15.5409 6.29824 15.1213 5.87868C14.7018 5.45912 14.1672 5.1734 13.5853 5.05765C13.0033 4.94189 12.4001 5.0013 11.8519 5.22836C11.3038 5.45543 10.8352 5.83994 10.5056 6.33329C10.1759 6.82664 10 7.40666 10 8C10 8.79565 10.3161 9.55871 10.8787 10.1213C11.4413 10.6839 12.2044 11 13 11ZM13 7C13.1978 7 13.3911 7.05865 13.5556 7.16853C13.72 7.27841 13.8482 7.43459 13.9239 7.61732C13.9996 7.80004 14.0194 8.00111 13.9808 8.19509C13.9422 8.38907 13.847 8.56726 13.7071 8.70711C13.5673 8.84696 13.3891 8.9422 13.1951 8.98079C13.0011 9.01937 12.8 8.99957 12.6173 8.92388C12.4346 8.84819 12.2784 8.72002 12.1685 8.55557C12.0586 8.39112 12 8.19778 12 8C12 7.73478 12.1054 7.48043 12.2929 7.29289C12.4804 7.10536 12.7348 7 13 7V7ZM17.11 10.86C17.6951 10.021 18.0087 9.02282 18.0087 8C18.0087 6.97718 17.6951 5.97897 17.11 5.14C17.3976 5.04741 17.6979 5.00018 18 5C18.7956 5 19.5587 5.31607 20.1213 5.87868C20.6839 6.44129 21 7.20435 21 8C21 8.79565 20.6839 9.55871 20.1213 10.1213C19.5587 10.6839 18.7956 11 18 11C17.6979 10.9998 17.3976 10.9526 17.11 10.86V10.86ZM13 13C7 13 7 17 7 17V19H19V17C19 17 19 13 13 13ZM9 17C9 16.71 9.32 15 13 15C16.5 15 16.94 16.56 17 17H9ZM24 17V19H21V17C20.9766 16.2566 20.8054 15.5254 20.4964 14.8489C20.1873 14.1724 19.7466 13.5643 19.2 13.06C24 13.55 24 17 24 17ZM8 12H5V15H3V12H0V10H3V7H5V10H8V12Z",
+      title: "Collaborative Learning",
+      description: "Teamwork is at the heart of our philosophy. Our platform fosters collaborative learning, enabling interns to work together on group projects. You'll gain not only technical skills but also the ability to thrive in a team environment."
+    },
+    {
+      iconPath: "M22 17H18V10H22V17ZM23 8H17C16.7348 8 16.4804 8.10536 16.2929 8.29289C16.1054 8.48043 16 8.73478 16 9V19C16 19.2652 16.1054 19.5196 16.2929 19.7071C16.4804 19.8946 16.7348 20 17 20H23C23.2652 20 23.5196 19.8946 23.7071 19.7071C23.8946 19.5196 24 19.2652 24 19V9C24 8.73478 23.8946 8.48043 23.7071 8.29289C23.5196 8.10536 23.2652 8 23 8ZM4 6H22V4H4C3.46957 4 2.96086 4.21071 2.58579 4.58579C2.21071 4.96086 2 5.46957 2 6V17H0V20H14V17H4V6Z",
+      title: "SDLC Techniques",
+      description: "Understanding the Software Development Life Cycle (SDLC) is pivotal in the tech world. Acquire skills that align with industry standards and boost your project management proficiency."
+    },
+    {
+      iconPath: "M14 10H2V12H14V10ZM14 6H2V8H14V6ZM2 16H10V14H2V16ZM21.5 11.5L23 13L16 20L11.5 15.5L13 14L16 17L21.5 11.5Z",
+      title: "Easy to understand",
+      description: "Learning shouldn't be complicated. Our tasks are designed to be easily comprehensible, ensuring a smooth learning experience for everyone. Whether you're a seasoned professional or a beginner."
+    }
+  ];
+
   return (
     <div className="Portal">
       <div className="portal-img">
-        <img ref={imgRef} src={task} alt="" />
+        <img ref={imgRef} src={task} alt="Task Portal" />
       </div>
       <div className="portal-text">
         <h5>Our own task portal</h5>
         <h1>Manage Project Via Own Task Portal</h1>
-        <p>
-          Welcome to internee.pk task portal. Where Tasks Transform Into Skills
-        </p>
+        <p>Welcome to internee.pk task portal. Where Tasks Transform Into Skills</p>
         <div className="options">
-          <div className="option">
-            <div className="icon" ref={addIconRef}>
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M2 3H10C10 2.46957 10.2107 1.96086 10.5858 1.58579C10.9609 1.21071 11.4696 1 12 1C12.5304 1 13.0391 1.21071 13.4142 1.58579C13.7893 1.96086 14 2.46957 14 3H22V5H21V16H15.25L17 22H15L13.25 16H10.75L9 22H7L8.75 16H3V5H2V3ZM5 5V14H19V5H5ZM11.85 11.85C11.76 11.94 11.64 12 11.5 12C11.3674 12 11.2402 11.9473 11.1464 11.8536C11.0527 11.7598 11 11.6326 11 11.5V7.5C11 7.36739 11.0527 7.24021 11.1464 7.14645C11.2402 7.05268 11.3674 7 11.5 7C11.64 7 11.76 7.06 11.85 7.15L13.25 8.54C13.57 8.86 13.89 9.18 13.89 9.5C13.89 9.82 13.57 10.14 13.25 10.46L11.85 11.85Z"
-                  fill="#43a724"
-                />
-              </svg>
+          {options.map((option, index) => (
+            <div className="option" key={index}>
+              <div className="icon" ref={addIconRef}>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d={option.iconPath} fill="#43a724" />
+                </svg>
+              </div>
+              <div className="option-txt">
+                <p>
+                  <b>{option.title}</b> {option.description}
+                </p>
+              </div>
             </div>
-            <div className="option-txt">
-              <p>
-                <b>Hands on Projects</b> we believe in learning by doing. Dive
-                into hands-on projects that simulate real-world scenarios. From
-                coding challenges to creative projects, every task is crafted to
-                impart practical skills that resonate in professional
-                environments.
-              </p>
-            </div>
-          </div>
-          <div className="option">
-            <div className="icon" ref={addIconRef}>
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M13 11C13.5933 11 14.1734 10.8241 14.6667 10.4944C15.1601 10.1648 15.5446 9.69623 15.7716 9.14805C15.9987 8.59987 16.0581 7.99667 15.9424 7.41473C15.8266 6.83279 15.5409 6.29824 15.1213 5.87868C14.7018 5.45912 14.1672 5.1734 13.5853 5.05765C13.0033 4.94189 12.4001 5.0013 11.8519 5.22836C11.3038 5.45543 10.8352 5.83994 10.5056 6.33329C10.1759 6.82664 10 7.40666 10 8C10 8.79565 10.3161 9.55871 10.8787 10.1213C11.4413 10.6839 12.2044 11 13 11ZM13 7C13.1978 7 13.3911 7.05865 13.5556 7.16853C13.72 7.27841 13.8482 7.43459 13.9239 7.61732C13.9996 7.80004 14.0194 8.00111 13.9808 8.19509C13.9422 8.38907 13.847 8.56726 13.7071 8.70711C13.5673 8.84696 13.3891 8.9422 13.1951 8.98079C13.0011 9.01937 12.8 8.99957 12.6173 8.92388C12.4346 8.84819 12.2784 8.72002 12.1685 8.55557C12.0586 8.39112 12 8.19778 12 8C12 7.73478 12.1054 7.48043 12.2929 7.29289C12.4804 7.10536 12.7348 7 13 7V7ZM17.11 10.86C17.6951 10.021 18.0087 9.02282 18.0087 8C18.0087 6.97718 17.6951 5.97897 17.11 5.14C17.3976 5.04741 17.6979 5.00018 18 5C18.7956 5 19.5587 5.31607 20.1213 5.87868C20.6839 6.44129 21 7.20435 21 8C21 8.79565 20.6839 9.55871 20.1213 10.1213C19.5587 10.6839 18.7956 11 18 11C17.6979 10.9998 17.3976 10.9526 17.11 10.86V10.86ZM13 13C7 13 7 17 7 17V19H19V17C19 17 19 13 13 13ZM9 17C9 16.71 9.32 15 13 15C16.5 15 16.94 16.56 17 17H9ZM24 17V19H21V17C20.9766 16.2566 20.8054 15.5254 20.4964 14.8489C20.1873 14.1724 19.7466 13.5643 19.2 13.06C24 13.55 24 17 24 17ZM8 12H5V15H3V12H0V10H3V7H5V10H8V12Z"
-                  fill="#43a724"
-                />
-              </svg>
-            </div>
-            <div className="option-txt">
-              <p>
-                <b>Collaborative Learning</b> teamwork is at the heart of our
-                philosophy. Our platform fosters collaborative learning, enabling
-                interns to work together on group projects. You'll gain not only
-                technical skills but also the ability to thrive in a team
-                environment.
-              </p>
-            </div>
-          </div>
-          <div className="option">
-            <div className="icon" ref={addIconRef}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M22 17H18V10H22V17ZM23 8H17C16.7348 8 16.4804 8.10536 16.2929 8.29289C16.1054 8.48043 16 8.73478 16 9V19C16 19.2652 16.1054 19.5196 16.2929 19.7071C16.4804 19.8946 16.7348 20 17 20H23C23.2652 20 23.5196 19.8946 23.7071 19.7071C23.8946 19.5196 24 19.2652 24 19V9C24 8.73478 23.8946 8.48043 23.7071 8.29289C23.5196 8.10536 23.2652 8 23 8ZM4 6H22V4H4C3.46957 4 2.96086 4.21071 2.58579 4.58579C2.21071 4.96086 2 5.46957 2 6V17H0V20H14V17H4V6Z" fill="#43a724"/>
-</svg>
-
-            </div>
-            <div className="option-txt">
-              <p>
-                <b>SDLC Techniques</b> Understanding the Software Development Life Cycle (SDLC) is pivotal in the tech world. Acquire skills that align with industry standards and boost your project management proficiency.
-              </p>
-            </div>
-          </div>
-          <div className="option">
-            <div className="icon" ref={addIconRef}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M14 10H2V12H14V10ZM14 6H2V8H14V6ZM2 16H10V14H2V16ZM21.5 11.5L23 13L16 20L11.5 15.5L13 14L16 17L21.5 11.5Z" fill="#43a724"/>
-</svg>
-
-            </div>
-            <div className="option-txt">
-              <p>
-                <b>Easy to understand</b> Learning shouldn't be complicated. Our tasks are designed to be easily comprehensible, ensuring a smooth learning experience for everyone. Whether you're a seasoned professional or a beginner.
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
